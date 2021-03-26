04e40fb7552bd5d875eae218864ff8d2e36ffd15
efrain-gonzalez <zs18003255 estudiantes.uv.mx>
2021-03-26 16:51:56 +0000

    operators.py

---
+++ operators.py
@@ -0,0 +1,22 @@
+age1 = 9.0
+age2 = 20.0
+
+print "age1 = ", age1
+print "age2 = ", age2
+
+sumage = age1 + age2
+print "Suma (+) : ", sumage
+print "Resta (-) : ", age1 - age2
+print "Multiplica (*) : " , age1 * age2
+print "Dividir (/) : ", age1 / age2
+print "Residuo (%) : ", age2 % age1
+
+message = "today was a beatiful day"
+firstname = "Efrain"
+lastname = "Gonzalez"
+
+print message
+print firstname
+print lastname
+
+print "sum  strings (+) ", message + " " + firstname + " " + lastname
