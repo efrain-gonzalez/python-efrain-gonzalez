9bb92b401d20c10c1b7abc0be71b86a28f733f10
efrain-gonzalez <zs18003255 estudiantes.uv.mx>
2021-03-26 16:25:52 +0000

    variables.py

---
+++ variables.py
@@ -0,0 +1,21 @@
+#variables.py
+#autor: efrain gonzalez
+# marzo 26, 2021
+
+year = 2021;
+sentence = " my name is efrain";
+sarah, bob, mike = 16, 20, 25;
+name, age = "maria", 24;
+mary = fer = 20;
+pi = 3.1416
+e = 2.71
+
+#display values
+print "year = ", year
+print sentence
+print "sara : ", sarah, "\n bob =", bob, "\n mike =", mike
+print "\n\t name =", name
+print "\n\t age =", age
+print mary, fer
+print pi
+print e
