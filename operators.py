a96dacf360d27848047cb9a7342ec4714ed6db21
efrain-gonzalez <zs18003255 estudiantes.uv.mx>
2021-04-07 18:46:10 +0000

    add operators.py

--- operators.py
+++ operators.py
@@ -20,3 +20,12 @@ print firstname
 print lastname
 
 print "sum  strings (+) ", message + " " + firstname + " " + lastname
+
+adn = "AACTGTGAACTAACGTCTTAACGACTGGTACGTAC"
+print "adn = ", adn
+print " adn pos 0 ", adn[0]
+print " adn pos 4 ", adn[4]
+print " adn pos 0 to 7 ", adn[0:7]
+print " adn pos 10 to 14 ", adn[10:14]
+print " adn first 10 ", adn[:10]
+print " adn length - 2 ", adn[:-2]
